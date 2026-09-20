2c6550a1777f972cc9594d2d0a43a7b18f235d45
Abhimanyu Singh Negi <avi2507.work@gmail.com>
2026-09-20 23:49:39 +0530

    Solve linear-regression-closed-form

--- linear-regression-closed-form/linear-regression-closed-form.py
+++ linear-regression-closed-form/linear-regression-closed-form.py
@@ -4,8 +4,6 @@ def linear_regression_closed_form(X: list, y: list) -> list:
     """
     Returns the optimal weight vector as a list.
     """
-    Xt = np.transpose(X)
-
-    w = np.dot(np.dot(np.linalg.inv(np.dot(Xt,X)),Xt),y)
-
-    return w
+    X=np.array(X)
+    s = np.linalg.inv(X.T @ X) @ X.T @ y
+    return s.tolist()
